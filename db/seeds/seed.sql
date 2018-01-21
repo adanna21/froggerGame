e9dcf799397530d97a02817cb40e3a8c47844714
Adanna Omatu <adaomatu@gmail.com>
2018-01-20 19:52:31 -0500

    updated seed file to include name

--- db/seeds/seed.sql
+++ db/seeds/seed.sql
@@ -1,28 +1,37 @@
-INSERT INTO users (score) VALUES
+INSERT INTO users (name, score) VALUES
   (
+    'china',
     4566
   ),
   (
+    'bucky',
     7899
   ),
   (
+    'jjj',
     6666
   ),
   (
+    'ppp',
     4444
   ),
   (
+    'ernie',
     999
   ),
   (
+    'aria',
     44
   ),
   (
+    'rob',
     99
   ),
   (
+    'sana',
     66
   ),
   (
+    'cersai',
     5
   )
