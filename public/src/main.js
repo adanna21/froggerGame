74ee7d2782e4131e40ddd012fc901506c7a96649
Adanna Omatu <adaomatu@gmail.com>
2018-01-25 02:07:35 -0500

    still no collision for water

--- public/src/main.js
+++ public/src/main.js
@@ -10,6 +10,7 @@ const $scorePage = $('#scoresContainer')
 const $play = $('#homeStartBtn')
 
 const mainSong = new Audio('assets/frogger-forever.mp3')
+let startTimer
 
 // play mainsong
 // mainSong.play()
@@ -19,6 +20,7 @@ $('#homeStartBtn').on('click', function () {
   mainSong.pause()
   $homePage.css('display', 'none')
   $gamePage.css('display', 'block')
+  startTimer = setInterval(function(){ timer() }, 1000)
     // mainSong.pause()
 })
 
@@ -44,10 +46,13 @@ $('#scores').on('click', function () {
 // SET CANVAS
 const canvas = document.getElementById('game-wrapper')
 const ctx = canvas.getContext('2d')
+let play
+let timeDisplayed = document.getElementById('time')
+let counter = 60
 
 // ----------------- SET SPRITE VARIBLES & IMAGES ---------------//
 // LOAD ALL IMAGES IN ONE OBJECT
-var Images = new function() {
+let Images = new function() {
   // IMAGES
   this.frog = new Image()
   this.truck = new Image()
@@ -69,7 +74,9 @@ var Images = new function() {
     if (numLoaded === numImages) {
       console.log(numLoaded)
       postionObjs()
+      play = true
       draw()
+      // let startTimer = setInterval(function(){ timer() }, 1000)
     }
   }
   this.frog.onload = function () {
@@ -116,11 +123,18 @@ var Images = new function() {
   this.pad.src = '../images/lilypad.png'
 }
 
+// frog position and size
 let x = 225
 // let y = 520
 let y = 230
 let width = 25
 let height = 25
+
+let lives = 3
+let lostLives = 0
+let frogs = document.getElementById('frog-lives')
+let frogImages = document.querySelectorAll('.frogs')
+console.log(frogs)
 // ---------------------------- MOVE FROG ----------------------//
 // KEY PRESS
 // key monitor
@@ -160,28 +174,28 @@ function drawFrog () {
 function moveFrog () {
   if (upPressed === true && up === true && y > -10) {
     // ctx.rotate(.5)
-    y -= 15
+    y -= 10
     up = false
   } else if (upPressed === false) {
     up = true
   }
 
   if (downPressed === true && down === true && y < 530) {
-    y += 15
+    y += 10
     down = false
   } else if (downPressed === false) {
     down = true
   }
 
   if (rightPressed === true && right === true && x < 515) {
-    x += 15
+    x += 10
     right = false
   } else if (rightPressed === false) {
     right = true
   }
 
   if (leftPressed === true && left === true && x > -15) {
-    x -= 15
+    x -= 10
     left = false
   } else if (leftPressed === false) {
     left = true
@@ -241,16 +255,16 @@ function postionObjs () {
   landObjsArr.push(truckObj, redcarObj, racecarObj, bulldozerObj, yellowRacecarObj)
 
   // initiate water objs
-  tinyLogObj = new ItemConstructor(Images.log, 60, 30, 1.6, 500, 155)
-  tinyLog2Obj = new ItemConstructor(Images.log, 60, 30, 1.6, 40, 155)
-  medLogObj = new ItemConstructor(Images.log, 80, 30, 1.3, 0, 115)
-  medLog2Obj = new ItemConstructor(Images.log, 80, 30, 1.3, 450, 115)
-  bigLogObj = new ItemConstructor(Images.log, 100, 30, 1.6, 300, 50)
-  bigLog2Obj = new ItemConstructor(Images.log, 100, 30, 1.6, 25, 50)
+  tinyLogObj = new ItemConstructor(Images.log, 60, 35, 1.6, 500, 195)
+  tinyLog2Obj = new ItemConstructor(Images.log, 60, 35, 1.6, 40, 195)
+  medLogObj = new ItemConstructor(Images.log, 80, 35, 1.3, 0, 115)
+  medLog2Obj = new ItemConstructor(Images.log, 80, 35, 1.3, 450, 115)
+  bigLogObj = new ItemConstructor(Images.log, 100, 35, 1.6, 300, 50)
+  bigLog2Obj = new ItemConstructor(Images.log, 100, 35, 1.6, 25, 50)
   twoTurtlesObj = new ItemConstructor(Images.twoTurtles, 75, 35, -1.4, 450, 80)
   twoTurtles2Obj = new ItemConstructor(Images.twoTurtles, 75, 35, -1.4, 50, 80)
-  threeTurtlesObj = new ItemConstructor(Images.threeTurtles, 110, 26, -1.4, 120, 200)
-  threeTurtles2Obj = new ItemConstructor(Images.threeTurtles, 110, 26, -1.4, 400, 200)
+  threeTurtlesObj = new ItemConstructor(Images.threeTurtles, 110, 35, -1.4, 120, 155)
+  threeTurtles2Obj = new ItemConstructor(Images.threeTurtles, 110, 35, -1.4, 400, 155)
 
   floatingObjsArr.push(tinyLogObj, medLogObj, bigLogObj, twoTurtlesObj, tinyLog2Obj, medLog2Obj, bigLog2Obj,threeTurtlesObj, twoTurtles2Obj, threeTurtles2Obj)
 
@@ -314,7 +328,8 @@ function carCollided () {
       elem.y + elem.height >= y &&
       elem.y <= y + height) {
         // console.log('detected!!!')
-        y = 530
+        removeLife()
+        lives--
         // pixelCollision(elem)
     }
   })
@@ -334,9 +349,6 @@ function pixelCollision (elem) {
       if ((frogObj.imageData.data[ frogpixel ] !== 0) &&
         (elem.imageData.data[ elempixel ] !== 0)) {
         console.log("pixel collision")
-        y = 530
-        x = 225
-        break
       }
     }
   }
@@ -407,26 +419,55 @@ function moveLogsTurtles () {
       threeTurtles2Obj.x = 700
   }
 }
-
+let ok
 function float () {
-  if (y < 230) {
+  if (y < 220) {
     floatingObjsArr.forEach(elem => {
       if (elem.x <= x + width &&
         elem.x + elem.width >= x &&
         elem.y + elem.height >= y &&
         elem.y <= y + height) {
-          if (x < 515 && x > -15) {
+          if (x < canvas.width - 10 && x > 0) {
             x += elem.speed
+            ok = true
+            // waterPixelCollision(elem)
             console.log('detected!!!')
           }
-          // y = 530
-          // pixelCollision(elem)
-      }
+      }else{console.log('in water') }
     })
+  }
+  
+}
+function check () {
+  ok = false
+  if(ok === false) {
+    y = 560
+  }
+}
+
+function waterPixelCollision (elem) {
+  // get area of collision intersection
+  var xMin = Math.max(x, elem.x)
+  var yMin = Math.max(y, elem.y)
+  var xMax = Math.min(x + width, elem.x + elem.width)
+  var yMax = Math.min(y + height, elem.y + elem.height)
+
+  for (var pixelX = xMin; pixelX < xMax; pixelX++) {
+    for (var pixelY = yMin; pixelY < yMax; pixelY++) {
+      var frogpixel = ((pixelX - x) + (pixelY - y) * width) * 4 + 3
+      var elempixel = ((pixelX - elem.x) + (pixelY - elem.y) * elem.width) * 4 + 3
+      if ((frogObj.imageData.data[ frogpixel ] !== 0) &&
+        (elem.imageData.data[ elempixel ] !== 0)) {
+          x += elem.speed
+        console.log("pixel collision")
+      }
+    }
   }
 }
 
 // ---------------------- PADS------------------------//
+let winScenario
+
 function drawPads () {
   padObjsArr.forEach(elem => {
     ctx.drawImage(elem.obj, elem.x, elem.y, elem.width, elem.height)
@@ -454,7 +495,58 @@ function onPad () {
       ctx.drawImage(Images.frog, elem.x, elem.y, width, height)
     }
   })
+
+  if(padObj.onPad &&
+    pad2Obj.onPad &&
+    pad3Obj.onPad &&
+    pad4Obj.onPad &&
+    pad5Obj.onPad) {
+      winScenario = true
+    }
 }
+
+// -------------------------------- GAME LOGIC -------------------------------------//
+
+// ----------- Remove Life  ----------//
+function removeLife (){
+  Images.frog.src = '../images/icons8-Poison-96.png'
+  frogImages[lives - 1].remove()
+  setTimeout(function(){
+    Images.frog.src = '../images/frog.png'
+  }, 700)
+  x = 225
+  y = 560
+  counter = 60
+}
+
+// ----------- Count Down Timer  --------//
+function timer () {
+  counter--
+  timeDisplayed.innerText = `${counter}`
+  // check if time is  has run out, if so remove a life
+  if (counter === 0) {
+    // dieSong.play()
+    removeLife()
+    lives--
+    counter = 60
+    timeDisplayed.style.color = '#eff200'
+  } else if (counter <= 20) {
+    timeDisplayed.style.color = 'red'
+  } 
+}
+
+// ----------- GAME OVER ---------//
+let enterNameMsg = document.getElementById('enterName')
+function gameOver () {
+  if(lives < 1) {
+    console.log('yoooo')
+    enterNameMsg.style.display = 'block'
+    clearInterval(startTimer)
+    play = false
+  }
+}
+// ------------ WIN -------------//
+
 // REQUEST ANIMATION FRAME
 function draw () {
   // stop images from repeating as they move
@@ -464,163 +556,18 @@ function draw () {
   drawFrog()
   moveFrog()
   drawCars()
-  // moveCars()
+  moveCars()
   moveLogsTurtles()
   onPad()
   float()
   carCollided()
-  requestAnimationFrame(draw)
+  gameOver()
+  if (play) {
+    requestAnimationFrame(draw)
+  
+  }
+  
 }
-
-// SET VARIABLES
-const $objects = $('.obj')
-// lillys
-const $lilly1 = $('#lilypad1')
-const $lilly2 = $('#lilypad2')
-const $lilly3 = $('#lilypad3')
-const $lilly4 = $('#lilypad4')
-const $lilly5 = $('#lilypad5')
-// turtles
-const $turtle1 = $('#turtle1')
-const $turtle2 = $('#turtle2')
-// logs
-const $smallLog = $('#small-log')
-const $medLog = $('#med-log')
-const $longLog = $('#long-log')
-// cars
-// const $lateralObjs = $('.lateral-moving')
-// const $truck = $('#truck')
-// const $redcar = $('#redcar')
-// const $racecar = $('#racecar')
-// const $bulldozer = $('#bulldozer')
-// const $yellowRacecar = $('#yellow-racecar')
-// const $frog = $('#frog')
-
-// Set initial position of all items
-// function setPosition (obj, x, y, z) {
-//   let translate = `translate3d(${x}, ${y}, ${z})`
-//   obj.css({
-//       '-ms-transform': translate,
-//       '-webkit-transform': translate,
-//       'transform': translate
-//     })
-// }
-
-// // lillys
-// setPosition($lilly1, '60px', '-15px', 0)
-// setPosition($lilly2, '160px', '-15px', 0)
-// setPosition($lilly3, '260px', '-15px', 0)
-// setPosition($lilly4, '360px', '-15px', 0)
-// setPosition($lilly5, '460px', '-15px', 0)
-
-// // turtles
-// setPosition($turtle1, '200px', '80px', 0)
-// setPosition($turtle2, '360px', '200px', 0)
-
-// // logs
-// setPosition($smallLog, '200px', '165px', 0)
-// setPosition($medLog, '360px', '130px', 0)
-// setPosition($longLog, '50px', '40px', 0)
-
-// setPosition($truck, '300px', '320px', 0)
-// setPosition($redcar, '350px', '430px', 0)
-// setPosition($racecar, '150px', '380px', 0)
-// setPosition($bulldozer, '250px', '470px', 0)
-// setPosition($yellowRacecar, '90px', '530px', 0)
-// setPosition($frog, '245px', '600px', 0)
-
-// /* Create a constructor that can encompass each item */
-// let itemConstructor = function (obj) {
-//   this.cssObj = obj[0]
-//   this.cssWidth = parseInt($(this.cssObj).css('width'))
-//   this.cssHeight = parseInt($(this.cssObj).css('height'))
-//   this.xmin = parseInt($(this.cssObj).css('transform').split(',')[4])// returns position X - left side
-//   this.posYmin = parseInt($(this.cssObj).css('transform').split(',')[5]) // returns position Y - top
-//   this.posXmax = this.posXmin + this.cssWidth // right
-//   this.posYmax = this.posYmin + this.cssHeight // bottom
-//   this.speed = -5
-//   this.updateSpeed = function () {
-//       this.posXmax = this.posXmax + this.speed
-//       this.posXmin = this.posXmin + this.speed
-//     }
-//   this.clientRect = function () {
-//       return this.cssObj.getBoundingClientRect()
-//     }
-// }
-
-// /* Instantiate moving frog */
-// let movingFrog = new itemConstructor($frog)
-
-// /* Global variable */
-// let landObjsArr = [] // will contain all lateral moving items
-
-// /* Define array of all lateral moving objects and instantiate them */
-// $lateralObjs.each(function (index) {
-//   landObjsArr.push(new itemConstructor($(this)))
-// })
-
-// $('#homeStartBtn').on('click', startGame)
-
-// function startGame () {
-// // diables continuous clicking of start button
-//   $('#homeStartBtn').on('click', startGame).off()
-
-// // set the interval for timer on page
-//   let setIntID = setInterval(timer, 1000)
-
-//   // let truckPosX = 300
-//   // let redcarPosX = 300
-//   // let racecarPosX = 150
-//   let frogX = 245
-//   let frogY = 600
-
-//   // ----------------- ANIMATE CARS  ---------------//
-//   function moveObj () {
-//     // decriment or increment each item by entered numbers
-//     // truckPosX -= 5
-//     // redcarPosX -= 6
-//     // racecarPosX += 6
-//     //
-//     landObjsArr.forEach(car => {
-//       car.updateSpeed()
-//       $(car.cssObj).css('transform', `translate3d(${car.posXmin}px, ${car.posYmin}px, 0px)`)
-//       if (car.posXmin < -5) {
-//         car.posXmin = 550
-//       }
-//       // call below function to check if there is collision
-//       checkForCrashFrog()
-//     })
-
-//     // checkForCrashCar()
-
-//     window.requestAnimationFrame(moveObj)
-//   }
-
-//   // ----------------- Count Down Timer  ---------------//
-//   let timeDisplayed = $('#time')
-//   let counter = 60
-
-//   // collect score
-
-//   // let sec = timeDisplayed.text(`${counter}`)
-//   function timer () {
-//     counter--
-//     timeDisplayed.text(`${counter}`)
-//       // check if time is  has run out, if so remove a life
-//     if (counter === 0) {
-//         dieSong.play()
-//         lives--
-//         $frogImages[lives].remove()
-//         setPosition($frog, '245px', '600px', 0)
-//         frogX = 245
-//         frogY = 600
-//         counter = 60
-//       } else if (counter <= 20) {
-//       timeDisplayed.css('color', 'red')
-//     } else if (counter > 20) {
-//       timeDisplayed.css('color', '#eff200')
-//     }
-//   }
 
 //   // ----------------- Collision Detection ---------------//
 //   let $frogImages = $('#frog-lives img')
@@ -683,62 +630,3 @@ const $longLog = $('#long-log')
 //         clearInterval(setIntID)
 //       }
 //     }// end of checkForCrashFrog
-
-//   // -------------------   Move Frog   ---------------//
-//   let $score = $('#score')
-//   let score = 0
-
-//   function moveFrog (event) {
-//          // when arrow keys are pressed move as advised
-//       switch (event.keyCode) {
-//         case 37:
-//           // make sure frog is within screen
-//           if (frogX <= 4) {
-//             event.preventDefault()
-//           } else{
-//             frogX -= 30 
-// }
-//           movingFrog.posXmin = frogX
-//           movingFrog.posXmax = movingFrog.posXmin + movingFrog.cssWidth
-//           // console.log(frogX)
-//           $frog.css('transform', `translate3d(${frogX}px, ${frogY}px, 0px)`)  // left
-//           break
-//         case 38:
-//           if (frogY <= 0) {
-//             event.preventDefault()
-//           }else {
-//             frogY -= 30 
-// }
-//           score += 10
-//           $score.text(`${score}`)
-//           movingFrog.posYmin = frogY
-//           movingFrog.posYmax = movingFrog.posYmin + movingFrog.cssHeight
-//           $frog.css('transform', `translate3d(${frogX}px, ${frogY}px, 0px)`) // up
-//           break
-//         case 39:
-//           if (frogX >= 500) {
-//             event.preventDefault()
-//           }else {
-//             frogX += 30 
-// }
-//           movingFrog.posXmin = frogX
-//           movingFrog.posXmax = movingFrog.posXmin + movingFrog.cssWidth
-//           $frog.css('transform', `translate3d(${frogX}px, ${frogY}px, 0px)`)  // right
-//           break
-//         case 40:
-//           if (frogY >= 600) {
-//             event.preventDefault()
-//           } else{
-//             frogY += 30
-//  }
-//           movingFrog.posYmin = frogY
-//           movingFrog.posYmax = movingFrog.posYmin + movingFrog.cssHeight
-//           $frog.css('transform', `translate3d(${frogX}px, ${frogY}px, 0px)`) // down
-//           break
-//       } // keyCode end
-//         // ----------------- CONDITIONS IN WATER ---------------//
-//     } // moveFrog end
-//   $(document).on('keydown', (e) => { moveFrog(e) })
-    
-//   window.requestAnimationFrame(moveObj)
-// }
